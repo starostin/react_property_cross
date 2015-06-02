e7119bb12cef5a56556791362d38fb6251254ef7
starostin <starostinoleg89@mail.ru>
2015-06-02 23:10:22 +0300

    Add browserify

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var concat = require('gulp-concat');
 gulp.task('browserify', function(){
     gulp.src('www/index.js')
         .pipe(browserify({transform: 'reactify'}))
-        .pipe(concat('main.js'))
+        .pipe(concat('index.js'))
         .pipe(gulp.dest('build'));
 });
 
