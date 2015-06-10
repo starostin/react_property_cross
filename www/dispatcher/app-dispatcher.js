2ae1a0e797075d4eb74a1482d4d1f8c2d9dbfe33
o.starostin <starostinoleg370@gmail.com>
2015-06-10 21:48:03 +0300

    Add flux, object-assign

---
+++ www/dispatcher/app-dispatcher.js
@@ -0,0 +1,13 @@
+var Dispatcher = require('./dispatcher');
+var assign = require('object-assign');
+
+var AppDispatcher = assign({}, Dispatcher.prototype, {
+    handleViewAction: function(action){
+        console.log('--------------action----------', action)
+        this.dispatch({
+            source: 'VIEW_ACTION',
+            action: action
+        })
+    }
+});
+module.exports = AppDispatcher;
