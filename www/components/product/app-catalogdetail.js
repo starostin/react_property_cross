c9d1ad8be980f9d5f792778546f23a45020f77e8
starostin <starostinoleg89@mail.ru>
2015-06-22 23:01:28 +0300

    Add organization

---
+++ www/components/product/app-catalogdetail.js
@@ -0,0 +1,10 @@
+/** @jsx React.DOM */
+var React = require('react');
+
+var CatalogDetail = React.createClass({
+    render: function(){
+        return (<h1>CatalogDetail</h1>);
+    }
+});
+
+module.exports = CatalogDetail;
